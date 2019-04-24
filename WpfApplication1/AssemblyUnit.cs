4d69638ad31882a989eca278ceb08397cba7ce99
polli2592 <polli2592@mail.ru>
2019-04-24 20:40:45 +0500

    Добавьте файлы проекта.

---
+++ WpfApplication1/AssemblyUnit.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace WpfApplication1
+{
+    public class AssemblyUnit
+    {
+        public string Description { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string Position { get; set; }
+
+        public Part Partunit { get; set; }
+
+        private bool? _isChecked;
+
+        public bool? IsChecked { get; set; }
+
+
+        public AssemblyUnit(string _description,string _code,string _name,string _position, Part _partunit)
+        {
+            Description = _description;
+            Code = _code;
+            Name = _name;
+            Position = _position;
+            Partunit = _partunit;
+
+        }
+
+       /* void SetIsChecked(bool? value, bool updateChildren, bool updateParent)
+        {
+            if (value == _isChecked)
+                return;
+
+            _isChecked = value;
+
+            if (updateChildren && _isChecked.HasValue)
+                this.Children.ForEach(c => c.SetIsChecked(_isChecked, true, false));
+
+            if (updateParent && _parent != null)
+                _parent.VerifyCheckState();
+
+            this.OnPropertyChanged("IsChecked");
+        }*/
+
+   /*     void VerifyCheckState()
+        {
+            bool? state = null;
+            for (int i = 0; i < this.Children.Count; ++i)
+            {
+                bool? current = this.Children[i].IsChecked;
+                if (i == 0)
+                {
+                    state = current;
+                }
+                else if (state != current)
+                {
+                    state = null;
+                    break;
+                }
+            }
+            this.SetIsChecked(state, false, true);
+        }*/
+
+
+    }
+
+    public enum Part
+    {
+        Detail = 4,
+        AssemblyUnit=1,
+        Kompleks=2,
+        VariableDate = 3
+
+    }
+    class BindingProxy : Freezable
+    {
+        //Override of Freezable
+        protected override Freezable CreateInstanceCore()
+        {
+            return new BindingProxy();
+        }
+        public object Data
+        {
+            get { return (object)GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
+        public static readonly DependencyProperty DataProperty = DependencyProperty.Register("Data", typeof(object), typeof(BindingProxy), new UIPropertyMetadata(null));
+    }
+
+
+    public class Column : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected internal void OnPropertyChanged(string propertyname)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+        }
+
+        public DataGridLength Width
+        {
+            get { return m_width; }
+            set { m_width = value; OnPropertyChanged("Width"); }
+        }
+        DataGridLength m_width;
+    }
+
+
+    public class PartSp
+    {
+        public Part PartSpec { get; set; }
+
+        public List<AssemblyUnit> Units { get; set; }
+
+        public List<Part> ChildPart { get; set; }
+
+        public bool? IsChecked { get; set; }
+
+    }
+}
---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<AssemblyUnit> units { get; set; }
+
+        public List<PartSp> Parts { get; set; }
+
+
+        public int ColumnWidhtDescription { get; set; }
+
+        public bool IsChecked { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            units=new List<AssemblyUnit>();
+            units.Add(new AssemblyUnit("Деталь 1","012301234501201","Наименование детали 1", "01",Part.Detail));
+            units.Add(new AssemblyUnit("Деталь 2", "012301234501202", "Наименование детали 2", "02", Part.Detail));
+            units.Add(new AssemblyUnit("Деталь 3", "012301234501205", "Наименование детали 3", "04", Part.Detail));
+            units.Add(new AssemblyUnit("Деталь 4", "012301234501203", "Наименование детали 4", "05", Part.Detail));
+            units.Add(new AssemblyUnit("Сборочная деталь 1", "00001234501202", "Наименование СД 1", "06", Part.AssemblyUnit));
+            units.Add(new AssemblyUnit("Сборочная деталь 2", "000012345012013", "Наименование СД 2", "06", Part.AssemblyUnit));
+            units.Add(new AssemblyUnit("Сборочная деталь 3", "00001234501205", "Наименование СД 3", "06", Part.AssemblyUnit));
+            units.Add(new AssemblyUnit("Сборочная деталь 4", "00001234501206", "Наименование СД 4", "06", Part.AssemblyUnit));
+            this.DataContext = units;
+            Parts = new List<PartSp>();
+            PartSp partDetail=new PartSp();
+            partDetail.PartSpec = Part.Detail;
+            units = new List<AssemblyUnit>();
+            units.Add(new AssemblyUnit("Деталь 1", "012301234501201", "Наименование детали 1", "01", Part.Detail));
+            units.Add(new AssemblyUnit("Деталь 2", "012301234501202", "Наименование детали 2", "02", Part.Detail));
+            units.Add(new AssemblyUnit("Деталь 3", "012301234501205", "Наименование детали 3", "04", Part.Detail));
+            units.Add(new AssemblyUnit("Деталь 4", "012301234501203", "Наименование детали 4", "05", Part.Detail));
+            partDetail.Units=new List<AssemblyUnit>();
+            partDetail.Units = units;
+            Parts.Add(partDetail);
+            PartSp partAssembly = new PartSp();
+            partAssembly.PartSpec = Part.AssemblyUnit;
+            units = new List<AssemblyUnit>();
+            units.Add(new AssemblyUnit("Сборочная деталь 1", "00001234501202", "Наименование СД 1", "06", Part.AssemblyUnit));
+            units.Add(new AssemblyUnit("Сборочная деталь 2", "000012345012013", "Наименование СД 2", "06", Part.AssemblyUnit));
+            units.Add(new AssemblyUnit("Сборочная деталь 3", "00001234501205", "Наименование СД 3", "06", Part.AssemblyUnit));
+            units.Add(new AssemblyUnit("Сборочная деталь 4", "00001234501206", "Наименование СД 4", "06", Part.AssemblyUnit));
+            partAssembly.Units = new List<AssemblyUnit>();
+            partAssembly.Units = units;
+            Parts.Add(partAssembly);
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            units.Add(new AssemblyUnit("", "", "", "", Part.Kompleks));
+            
+        }
+    }
+}
